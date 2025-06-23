73a8a68a8211aa1e42d18bb18b1a1a177c0ccf66
Urlichtryy <1181934749@qq.com>
2025-06-23 18:39:33 +0800

    hw04解答

--- HW/hw04/hw04.py
+++ HW/hw04/hw04.py
@@ -13,6 +13,12 @@ def shuffle(s):
     """
     assert len(s) % 2 == 0, 'len(seq) must be even'
     "*** YOUR CODE HERE ***"
+    num=len(s)//2
+    result=[]
+    for i in range(num):
+        result.append(s[i])
+        result.append(s[i+num])
+    return result
 
 
 def deep_map(f, s):
@@ -38,6 +44,12 @@ def deep_map(f, s):
     True
     """
     "*** YOUR CODE HERE ***"
+    for i in range(len(s)):
+        if type(s[i]) is list:
+            deep_map(f, s[i])
+        else:
+            s[i] = f(s[i])
+
 
 
 HW_SOURCE_FILE=__file__
@@ -47,11 +59,13 @@ def planet(mass):
     """Construct a planet of some mass."""
     assert mass > 0
     "*** YOUR CODE HERE ***"
+    return ['planet',mass]
 
 def mass(p):
     """Select the mass of a planet."""
     assert is_planet(p), 'must call mass on a planet'
     "*** YOUR CODE HERE ***"
+    return p[1]
 
 def is_planet(p):
     """Whether p is a planet."""
@@ -104,6 +118,13 @@ def balanced(m):
     True
     """
     "*** YOUR CODE HERE ***"
+    if is_planet(m):
+        return True
+    else:
+        left_end=end(left(m))
+        right_end=end(right(m))
+        return length(left(m))*total_mass(left_end)==length(right(m))*total_mass(right_end) and balanced(left_end) and balanced(right_end)
+
 
 
 def berry_finder(t):
@@ -124,7 +145,13 @@ def berry_finder(t):
     True
     """
     "*** YOUR CODE HERE ***"
-
+    if label(t)=='berry':
+        return True
+    else:
+        for b in branches(t):
+            if berry_finder(b):
+                return True
+        return False
 
 HW_SOURCE_FILE=__file__
 
@@ -139,6 +166,12 @@ def max_path_sum(t):
     17
     """
     "*** YOUR CODE HERE ***"
+    if is_leaf(t):
+        return label(t)
+    max_b=0
+    for b in branches(t):
+        max_b = max(max_b, max_path_sum(b))
+    return max_b+label(t)
 
 
 def mobile(left, right):
